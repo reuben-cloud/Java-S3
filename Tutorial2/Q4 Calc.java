50f4311da2240d8693b25bca32bf0e32f8740643
Reuben Thomas <ujohn486@gmail.com>
2026-08-04 13:59:06 +0530

    Q4 Calc.java

---
+++ Tutorial2/Q4 Calc.java
@@ -0,0 +1,34 @@
+package M1.tut2;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter operator (+,-,*,/): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
