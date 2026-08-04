c39f9396c805b951d6a7b0b9df6d26a4abc2b664
Reuben Thomas <ujohn486@gmail.com>
2026-08-04 13:59:38 +0530

    Q5 Fibonacci.java

---
+++ Tutorial2/Q5 Fibonacci.java
@@ -0,0 +1,20 @@
+package M1.tut2;
+import java.util.Scanner;
+public class Fibonacci {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter n: ");
+	        int n = sc.nextInt();
+
+	        int a = 0, b = 1;
+
+	        for (int i = 1; i <= n; i++) {
+	            System.out.print(a + " ");
+	            int c = a + b;
+	            a = b;
+	            b = c;
+	        }
+	    }
+	}
