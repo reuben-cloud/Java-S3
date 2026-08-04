2172f4fc819d0953ef77e53be4d6c30f32dad38b
Reuben Thomas <ujohn486@gmail.com>
2026-08-04 13:57:13 +0530

    Q2 oddnos.java

---
+++ Tutorial2/Q2 oddnos.java
@@ -0,0 +1,18 @@
+package M1.tut2;
+import java.util.Scanner;
+public class oddnos {
+
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter n: ");
+	        int n = sc.nextInt();
+
+	        for (int i = 1; i <= n; i++) {
+	            if (i % 2 == 0)
+	                continue;
+	            System.out.print(i + " ");
+	        }
+	    }
+	}
