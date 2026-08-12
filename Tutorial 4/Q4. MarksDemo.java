445869bb1b37abfd395e8576ae717174d6f8b925
Reuben Thomas <ujohn486@gmail.com>
2026-08-13 00:06:49 +0530

    Q4. MarksDemo.java

---
+++ Tutorial 4/Q4. MarksDemo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Marks {
+    int mark1, mark2, mark3;
+
+    Marks(int mark1, int mark2, int mark3) {
+        this.mark1 = mark1;
+        this.mark2 = mark2;
+        this.mark3 = mark3;
+    }
+
+    int total() {
+        return mark1 + mark2 + mark3;
+    }
+
+    double average() {
+        return total() / 3.0;
+    }
+}
+
+public class MarksDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter mark 1: ");
+        int m1 = sc.nextInt();
+
+        System.out.print("Enter mark 2: ");
+        int m2 = sc.nextInt();
+
+        System.out.print("Enter mark 3: ");
+        int m3 = sc.nextInt();
+
+        Marks m = new Marks(m1, m2, m3);
+
+        System.out.println("Total = " + m.total());
+        System.out.println("Average = " + m.average());
+    }
+}
