dcf7927791441280bdca90da90efb86e6f20ef07
Reuben Thomas <ujohn486@gmail.com>
2026-08-13 00:12:17 +0530

    CountEvenOdd.java

---
+++ Tutorial 3/CountEvenOdd.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements (n): ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0, oddCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Total Even Elements: " + evenCount);
+        System.out.println("Total Odd Elements: " + oddCount);
+
+        sc.close();
+    }
+}
